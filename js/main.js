be189513976cb875cac3f6eeec92f32c711583a6
Sean Ballew <sballew7@gmail.com>
2019-05-05 19:19:03 -0700

    rebuilding site Sun, May  5, 2019  7:19:03 PM

--- js/main.js
+++ js/main.js
@@ -1,14 +1,55 @@
-/**
- * Created by fabiomadeira on 25/02/15.
- */
-// jQuery for page scrolling feature
-jQuery(document).ready(function(e) {
-    e(".scroll").click(function(t) {
-        t.preventDefault();
-        e("html,body").animate({
-            scrollTop: e(this.hash).offset().top
-        }, 1e3)
-    })
+/*
+*=================================
+* Hugo UILite Portfolio v0.8
+*=================================
+*
+* Free version https://uicard.io/products/hugo-uilite
+* Pro version https://uicard.io/products/hugo-uilite-pro
+* Demo https://demo.uicard.io/hugo-uilite-portfolio-demo/
+*
+* Coded By UICardio
+*
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*
+*/
+
+
+let menuBtn = $("#menuBar");
+
+menuBtn.click(function(){
+
+  $('.hamburger-menu').toggleClass('animate');
+
+  if($(".secondaryMenu").hasClass("active")){
+    
+    $(".secondaryMenu").removeClass("active");
+    setTimeout(function(){
+      $(".primaryMenu").addClass("active");
+    },400);
+
+    
+  } else {
+    $(".primaryMenu").removeClass("active");
+  
+    setTimeout(function(){
+      $(".secondaryMenu").addClass("active");
+    },350);
+  }
 });
 
+$(document).ready(function(){
+  var elements = $(".sidebar > .main-info *");
 
+  console.log(elements);
+
+  for(let i = 0; i < elements.length; i++){
+    setTimeout(function(){
+      $(elements[i].tagName).addClass("bs");
+    }, (400 * i) - 90 * i );
+  }
+
+  setTimeout(function(){
+    $(".main-content").addClass("active");
+  }, 1900);
+
+});
